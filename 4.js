34cb803ae09b01762a100e27a726a900c14467bc
maRktutuzhe <116672312+maRktutuzhe@users.noreply.github.com>
2023-01-24 14:15:52 +0500

    Add files via upload

---
+++ 4.js
@@ -0,0 +1,11 @@
+function print(a, b) {
+    let i = a;
+    let timerId = setInterval(function() {
+        console.log(i);
+        if (i == b) {
+            clearInterval(timerId);
+        }
+        i++;
+    }, 1000);
+}
+print(5, 15)
